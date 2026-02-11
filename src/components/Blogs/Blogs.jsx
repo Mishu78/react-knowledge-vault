22286a0256a9e43518b90d3c6c6af09323e560d8
programming hero <meherun2002@gmail.com>
2026-02-11 19:13:04 +0600

    proptypes added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -4,13 +4,13 @@ const Blogs = () => {
     //Data load to empty array
     const [blogs,setBlogs]=useState([]);
     useEffect(()=>{
-        fetch('blog.json')
+        fetch('/blog.json')
         .then(res=>res.json())
         .then(data=>setBlogs(data))
     },[])
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h2 className="text-4xl">Blogs: {blogs.length}</h2>
         </div>
     );
 };
