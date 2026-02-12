c90779a31f4cd0787798292946922920bd784cd0
programming hero <meherun2002@gmail.com>
2026-02-12 16:57:17 +0600

    author image and reading time added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
- const {title,cover} = blog
+ const {title,cover,author,author_img,reading_time} = blog
     return (
         <div>
+             <img src={cover} alt="" />
+             <div className="flex justify-between">
+                <div>
+                <img className="w-6"src={author_img} alt="" />
+                <div>
+
+                </div>
+                </div>
+                <div>
+                <span>{reading_time} min read</span>
+                </div>
+             </div>
             <h2 className="text-4xl">{title}</h2>
-           <img src={cover} alt="" />
+          
         </div>
     );
 };
