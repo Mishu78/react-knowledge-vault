e96a71ca40aac2fe1d7256aec86a736a88990ee4
programming hero <meherun2002@gmail.com>
2026-02-14 23:24:06 +0600

    commited by adding comments

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
  const {title,cover,author,author_img,reading_time,posted_date,hashtags} = blog
     return (
-        <div>
-             <img src={cover} alt="" />
+        <div className="mb-20">
+             <img className="w-full mb-8" src={cover} alt="" />
              <div className="flex justify-between">
-                <div className="flex">
+                <div className="flex mb-4">
                 <img className="w-6"src={author_img} alt="" />
-                <div className="ml-6">
-                <h3 className="text-2xl">{author}</h3>
+                <div className="ml-6 rounded-xs">
+                <h3 className="text-2xl rounded-xs">{author}</h3>
                 <p>{posted_date}</p>
                 </div>
                 </div>
@@ -16,7 +16,8 @@ const Blog = ({blog}) => {
                 <span>{reading_time} min read</span>
                 </div>
              </div>
-            <h2 className="text-4xl">{title}</h2>
+            <h2 className="text-4xl mb-4">{title}</h2>
+           
            <p>
             {
                 hashtags.map((hash,idx)=> <span key={idx}><a href="">#{hash}</a></span>)
@@ -25,6 +26,7 @@ const Blog = ({blog}) => {
         </div>
     );
 };
+ //show title 
 //proptype is used to check are we receiving the correct data or data type correct or is required or not.
 //proptype used in blog component because in this component a prop receiving as object .
 Blog.propTypes = {
